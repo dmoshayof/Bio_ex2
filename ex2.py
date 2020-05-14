baaea8a6a7437f1cbe20c2dcb7c324612231227e
daniel evelyne moshayof <danielevelyne.moshayof@live.biu.ac.il>
2020-05-14 12:41:32 +0300

    Add documentation

--- ex2.py
+++ ex2.py
@@ -1,18 +1,16 @@
 import random
 from copy import deepcopy as dc
-
 import numpy as np
-
 from Bio_ex2 import show_image
 
 ADJ_PATH = "Bio_ex2/adjMatrix.csv"
 adjMatrix = np.loadtxt(ADJ_PATH, delimiter=",")
 COLORS = ['G', 'R', 'B', 'L']
-START_FIT = 0
 NUM_OF_NODES = 12
 
 
 class Engine:
+
     def __init__(self, graph, parameters):
         self.graph = graph
         self.population_size = parameters['population_size']
@@ -23,6 +21,13 @@ class Engine:
         self.num_iter = parameters['number_of_iterations']
 
     def run(self):
+        """ Run the Engine methods:
+        # initialize the population
+        #reproduce
+        #crossover
+        #mutate
+        show the best solution image
+        """
         random.seed()
         population = self._initialize_population()
         for i in range(self.num_iter):
@@ -30,38 +35,47 @@ class Engine:
             new_crossovers = self.crossover(rest_population)
             population = top_solutions + new_crossovers
             self.mutate(population)
-            print(avg_fitness, best_fitness[1],len(population))
+            print(avg_fitness, best_fitness[1], len(population))
             colors_map = show_image.set_coloring_for_image(best_fitness)
             show_image.image(colors_map)
             if best_fitness[1] == 0:
                 break
 
-    def mutate(self,population):
+    def mutate(self, population):
+        """
+        for specific probability mutate P random genes.
+        :param population: the current population
+        :return: mutate genes in population
+        """
         for i in range(self.mutation_probability):
             gene = random.choice(population)
             rand_vertex = random.choice(list(gene[0].keys()))
             gene[0][rand_vertex] = random.choice(COLORS)
             fitness = self._calc_fitness(gene[0])
-            new_gene = (gene[0],fitness)
+            new_gene = (gene[0], fitness)
             population[population.index(gene)] = new_gene
 
-
-
-    def crossover(self,population):
+    def crossover(self, population):
+        """
+        Crossover between two genes in population.
+        Pick random slicing of the gene1 and replace with gene2
+        :param population: population: the current population
+        :return: crossover in population
+        """
         new_population = []
         for gene1 in population:
             gene2 = random.choice(population)
-            cutoff = random.randint(0,self.num_of_vertex)
+            cutoff = random.randint(0, self.num_of_vertex)
             coloring = {}
             count = 0
-            for v,color in gene1[0].items():
+            for v, color in gene1[0].items():
                 if count == cutoff:
                     break
                 coloring[v] = color
-                count +=1
+                count += 1
             count = 0
             for v, color in gene2[0].items():
-                count +=1
+                count += 1
                 if count < cutoff:
                     continue
                 coloring[v] = color
@@ -69,13 +83,17 @@ class Engine:
             new_population.append((coloring, fitness))
         return new_population
 
-
     def _initialize_population(self):
+        """
+        Initializing population_size of Nodes with random colors
+        and calculation of fitness for each individual
+        :return: list of tuples (coloring, fitness) where coloring = {vertex: color}
+        """
         random.seed()
         population = []
         while len(population) < self.population_size:
             coloring = {}
-            for v in graph.nodes.values():
+            for v in self.graph.nodes.values():
                 chosen_color = random.choice(COLORS)
                 coloring[v] = chosen_color
             fitness = self._calc_fitness(coloring)
@@ -83,6 +101,11 @@ class Engine:
         return population
 
     def _pick_best(self, population):
+        """
+        Pick the best solution according to fitness and slicing the population for reproduce
+        :param population: current population
+        :return: average fitness of population, best solution in current population, top solutions, all others.
+        """
         population.sort(key=lambda x: x[1])
         fitnesses = [f[1] for f in population]
         avg = sum(fitnesses) / len(fitnesses)
@@ -92,6 +115,11 @@ class Engine:
         return avg, population[0], top_solutions, rest
 
     def _calc_fitness(self, coloring):
+        """
+        Calculation of the current fitness of each coloring based on sef fitness of each node
+        :param coloring: dict {vertex : color)
+        :return: int fitness
+        """
         fitness = 0
         for v, color in coloring.items():
             fitness += v.self_fitness(coloring, color)
@@ -102,17 +130,19 @@ class Graph:
     def __init__(self, adjMatrix):
         self.adjMatrix = adjMatrix
         self.nodes = {}
+        self.build_nodes()
+
 
     def build_nodes(self):
+        """
+        Build the graph nodes with neighbours according to adj Matrix
+        :return:
+        """
         for i in range(NUM_OF_NODES):
             result = np.where(self.adjMatrix[i, :] == 1)
             n = np.add(result, 1)
             node_i = Node(ID=i + 1, neighbours=n)
             self.nodes[i + 1] = dc(node_i)
-        for node in self.nodes.values():
-            n_array = np.array(node.neighbours_index)
-            for n in n_array:
-                node.set_neighbours(self.nodes.get(n))
 
 
 class Node:
@@ -120,12 +150,15 @@ class Node:
     def __init__(self, ID, neighbours):
         self.ID = ID
         self.neighbours_index = neighbours[0]
-        self.neighbours_obj = {}
-
-    def set_neighbours(self, node_n):
-        self.neighbours_obj[node_n.ID] = node_n
 
     def self_fitness(self, coloring, this_color):
+        """
+        Check for every node in coloring if it's a neighbour of this node and
+        check if they have same color, if do- count.
+        :param coloring: dict {node : color}
+        :param this_color:
+        :return: int self fitness
+        """
         count = 0
         for v, color in coloring.items():
             if v.ID in self.neighbours_index:
@@ -146,7 +179,12 @@ params = {
     'top_solutions_probability': 0.05
 }
 
-graph = Graph(adjMatrix)
-graph.build_nodes()
-engine = Engine(graph, params)
-engine.run()
+
+def main():
+    graph = Graph(adjMatrix)
+    engine = Engine(graph, params)
+    engine.run()
+
+
+if __name__ == '__main__':
+    main()
